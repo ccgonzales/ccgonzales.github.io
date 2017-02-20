997757737c980d001cc94d6baa2dfc7a04c4a966
Chris Gonzales <ccgonzales@gmail.com>
2017-02-20 16:52:16 -0500

    switch to bootstrap-4

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,39 @@
-console.log('This would be the main JS file.');
+
+var modal = $( ".foobar" ).dialog( "modal" );
+
+function insert_modal(content) {
+
+  return $('<div></div)').attr('id', 'modal').html(content).appendTo('body');
+}
+
+function delete_modal() {
+  $('body').find('modal').remove();
+}
+
+$(function() {
+
+  $('[data-modal]').on('click', function(event) {
+    var element = event.target || event.srcElement;
+    var modal = insert_modal($(element).data('modal'));
+  modal.dialog({
+    height: 435,
+    width:680,
+    close: function(event, ui) {
+      modal.remove();
+    }
+  });
+  });
+
+  $('[data-image]').on('click', function(event) {
+    var element = event.target || event.srcElement;
+    var modal = insert_modal($('<img>').attr('src', $(element).attr('src')));
+  modal.dialog({
+    height: $(element).prop('naturalHeight') + 70,
+    width:$(element).prop('naturalWidth') + 40,
+    close: function(event, ui) {
+      modal.remove();
+    }
+  });
+  });
+
+})
